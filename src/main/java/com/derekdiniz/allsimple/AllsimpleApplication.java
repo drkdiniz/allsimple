a4a053de4038fb007c01e3844cb43c3dd809210d
Derek Diniz <drk.diniz@gmail.com>
2024-06-18 14:25:38 -0300

    configured database

--- src/main/java/com/derekdiniz/allsimple/AllsimpleApplication.java
+++ src/main/java/com/derekdiniz/allsimple/AllsimpleApplication.java
@@ -1,13 +1,13 @@
-package com.derekdiniz;
+package com.derekdiniz.allsimple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodosimpleApplication {
+public class AllsimpleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodosimpleApplication.class, args);
+		SpringApplication.run(AllsimpleApplication.class, args);
 	}
 
 }
--- src/test/java/com/derekdiniz/allsimple/AllsimpleApplicationTests.java
+++ src/test/java/com/derekdiniz/allsimple/AllsimpleApplicationTests.java
@@ -1,10 +1,10 @@
-package com.derekdiniz;
+package com.derekdiniz.allsimple;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TodosimpleApplicationTests {
+class AllsimpleApplicationTests {
 
 	@Test
 	void contextLoads() {
