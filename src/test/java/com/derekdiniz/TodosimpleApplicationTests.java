f3026911b2c6b82df70b81dabe09afe0c76f1963
Derek Diniz <drk.diniz@gmail.com>
2024-06-18 12:54:44 -0300

    first commit

---
+++ src/test/java/com/derekdiniz/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derekdiniz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
