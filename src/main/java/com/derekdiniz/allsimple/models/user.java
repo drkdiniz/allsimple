6fde43765fa10a0f51747bcd766c29cc99ae92f0
Derek Diniz <drk.diniz@gmail.com>
2024-06-19 17:48:29 -0300

    Adicionado Hashcode and Equals

--- src/main/java/com/derekdiniz/allsimple/models/user.java
+++ src/main/java/com/derekdiniz/allsimple/models/user.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 
+
 @Entity
 @Table(name = "user")
 public class user {
@@ -36,4 +37,72 @@ public class user {
     @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
     @Size(groups = {CreateUser.class, UpdateUser.class }, min = 8, max = 60)
     private String password;
+
+    //private List<Task> tasks = new ArrayList<Task>();
+
+
+    public user() {
+    }
+
+    public user(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        user other = (user) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
+
+    
+
 }
